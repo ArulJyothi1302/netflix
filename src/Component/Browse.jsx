b19117ff8bf83c003c9eaa74fddac6f00957d511
Arul <hariarul2002@gmail.com>
2024-08-29 11:34:52 +0530

    bugfix

--- src/Component/Browse.jsx
+++ src/Component/Browse.jsx
@@ -6,8 +6,16 @@ const Browse = () => {
     <>
       <div className="">
         <Header />
-
-        <h1 className="text-2xl">Browse</h1>
+      </div>
+      <div className="absolute top-20 right-10 left-10 flex flex-col justify-center items-center">
+        <h1 className="">Hello</h1>
+        <h2>Welcome to Netflix</h2>
+        <form>
+          <fieldset className="p-2 bg-gray-400 rounded-lg">
+            <label className="p-2 bg-slate-100 rounded-lg m-1">Name:</label>
+            <input className="p-2 bg-slate-500 rounded-lg" type="text " />
+          </fieldset>
+        </form>
       </div>
     </>
   );
